71e12619ef16f9dfe027550e516ada55e9a34f8c
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-11 18:18:53 +0600

    update player container
    home page will show 6 players by default, pressing show more, more players will be visible.

--- src/components/PlayersContainer.jsx
+++ src/components/PlayersContainer.jsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router";
 import PlayerCard from "./PlayerCard";
 
 const PlayersContainer = () => {
   const players = useLoaderData();
+  const [displayPlayers, setDisplayPlayers] = useState([]);
+  const [showAll, setShowAll] = useState(false);
+
+  useEffect(() => {
+    if (showAll) {
+        setDisplayPlayers(players);
+    } else {
+        setDisplayPlayers(players.slice(0, 6));
+    }
+  },[players, showAll]);
 
   return (
     <div className="mt-15">
       <h1 className="text-2xl font-bold font-grotesk">Players</h1>
       <div className="grid grid-cols-1 mt-4 gap-5">
         {
-            players.map((player) => (
+            displayPlayers.map((player) => (
                 <PlayerCard key={player.id} player={player} />
             ))
         }
+      </div>
+      <div className="flex items-center justify-center mt-4 lg:mt-12">
+        <button
+            type="submit"
+            onClick={() => {
+                setShowAll((prv) => !prv);
+                if (showAll) {
+                    window.scrollTo(0, 2500);
+                }
+            }}
+            className="btn btn-neutral"
+        >
+            {showAll ? "View Less Players" : "View All Players"}
+        </button>
       </div>
     </div>
   );
