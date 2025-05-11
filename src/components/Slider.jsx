0e814d53697bb37e5cdc9c5c52c4b5d7ebeab0c1
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-11 13:08:42 +0600

    updated the slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,13 +4,18 @@ const Slider = () => {
     return (
         <div>
             <div className="carousel carousel-center rounded-b-box shadow-lg">
-                <div className="carousel-item">
+                {/* <div className="carousel-item">
                     <img className='w-70 h-100 object-cover' src="https://i.ibb.co.com/N64qZPHH/profile-pic.jpg" alt="Profile Picture" />
+                </div> */}
+                <div className="carousel-item">
+                    <img className='w-120 h-100 object-cover' src="https://i.ibb.co.com/4rjZPJF/flag.jpg" alt="Flag photo" />
                 </div>
                 <div className="carousel-item">
                     <img className='h-100' src="https://i.ibb.co.com/tPMKyLRH/group-photo-1.jpg" alt="Group Photo 1" />
                 </div>
-                
+                <div className="carousel-item">
+                    <img className='w-180 h-100 object-cover' src="https://i.ibb.co.com/d0kF76CD/group-photo-2.jpg" alt="Group Photo 2" />
+                </div>
             </div>
         </div>
     );
