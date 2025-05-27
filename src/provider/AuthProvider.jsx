65ca00adbddffd6cb0849ece40cf010e799c18f1
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-28 02:33:06 +0600

    refactor: enhance authentication provider with user login and logout functionality

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from '../firebase/firebase.init';
 
 const AuthProvider = ({ children }) => {
@@ -13,12 +13,53 @@ const AuthProvider = ({ children }) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
+    const loginUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+      return signOut(auth);
+    };
+
+    //fetch user data from server
+    const fetchUserData = (email) => {
+      fetch(`http://localhost/users/${email}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setUser(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoading(false);
+        });
+    };
+
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+
+        if (currentUser?.email) {
+          fetchUserData(currentUser.email);
+        } else {
+          setUser(null);
+        }
+        setLoading(false);
+      });
+      return () => {
+        unsubscribe();
+      };
+    }, []);
+
     const authData = {
         user,
         setUser,
         loading,
         setLoading,
         createUser,
+        loginUser,
+        logOut,
     }
 
     return (
