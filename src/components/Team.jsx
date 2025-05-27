c53d2cf99d466908b1bfb21c6d73e68cc6bf5698
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-27 23:40:31 +0600

    Fix team manager names and update images in Team component

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -9,14 +9,19 @@ const Team = () => {
                 <div className='grid grid-cols-1 lg:grid-cols-2 mt-4 gap-5 lg:gap-10'>
                     <div className='bg-base-300 rounded-lg shadow-lg'>
                         <img className='w-90 h-100 object-cover mx-auto my-4 lg:my-6 rounded-lg' src="https://i.ibb.co.com/MjqFrGp/manager-titash.jpg" alt="Manager 1" />
-                        <h2 className='font-grotesk text-2xl lg:text-3xl font-bold text-center mt-2'>Kamruzzan Titash</h2>
+                        <h2 className='font-grotesk text-2xl lg:text-3xl font-bold text-center mt-2'>Kamruzzaman Titash</h2>
                         <p className='font-grotesk lg:text-xl text-gray-600 text-center mt-2 mb-5'>Manager</p>
                     </div>
                     <div className='bg-base-300 rounded-lg shadow-lg'>
-                        <img className='w-90 h-100 object-cover mx-auto my-4 lg:my-6 rounded-lg' src="https://i.ibb.co.com/1Jppk33m/manager-shihan.jpg" alt="Manager 1" />
-                        <h2 className='font-grotesk text-2xl lg:text-3xl font-bold text-center mt-2'>Tahidul Shihan</h2>
+                        <img className='w-90 h-100 object-cover mx-auto my-4 lg:my-6 rounded-lg' src="https://i.ibb.co/G3pP8yJj/manager-monjur.jpg" alt="Manager 2" />
+                        <h2 className='font-grotesk text-2xl lg:text-3xl font-bold text-center mt-2'>Monzur Arafat</h2>
                         <p className='font-grotesk lg:text-xl text-gray-600 text-center mt-2 mb-5'>Manager</p>
                     </div>
+                    {/* <div className='bg-base-300 rounded-lg shadow-lg'>
+                        <img className='w-90 h-100 object-cover mx-auto my-4 lg:my-6 rounded-lg' src="https://i.ibb.co.com/1Jppk33m/manager-shihan.jpg" alt="Manager 3" />
+                        <h2 className='font-grotesk text-2xl lg:text-3xl font-bold text-center mt-2'>Tahidul Shihan</h2>
+                        <p className='font-grotesk lg:text-xl text-gray-600 text-center mt-2 mb-5'>Manager</p>
+                    </div> */}
                 </div>
             </div>
             {/* <div className='mt-15'>
